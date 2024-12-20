7f25f31a38453789aa513150f3865d15ba5e1ce7
surak-alf <suraprincekb@gmail.com>
2024-12-20 15:33:51 +0300

    change

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -48,25 +48,3 @@ def load_data_from_postgres(query):
 
 
 
-def load_data_using_sqlalchemy(query):
-    """
-    Connects to the PostgreSQL database and loads data based on the provided SQL query using SQLAlchemy.
-
-    :param query: SQL query to execute.
-    :return: DataFrame containing the results of the query.
-    """
-    try:
-        # Create a connection string
-        connection_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-        # Create an SQLAlchemy engine
-        engine = create_engine(connection_string)
-
-        # Load data into a pandas DataFrame
-        df = pd.read_sql_query(query, engine)
-
-        return df
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
